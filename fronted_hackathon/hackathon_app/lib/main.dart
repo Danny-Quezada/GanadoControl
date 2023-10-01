16fbe5d0d4a32a37eee3eda31cbbe3bba3668d53
Danny-Quezada <danny.quezada32u@std.uni.edu.ni>
2023-10-01 10:13:29 -0600

    Agregando nombre, icono, splash screen a la app

--- fronted_hackathon/hackathon_app/lib/main.dart
+++ fronted_hackathon/hackathon_app/lib/main.dart
@@ -1,4 +1,5 @@
 
+import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
 import 'package:flutter/material.dart';
 
 
@@ -80,7 +81,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         initialRoute: "/",
-        routes: {"/":(context)=>InitialPage()},
+        routes: {"/":(context)=>SplashScreen()},
         theme: ThemeData(
           primaryColor: ColorPalette.colorPrincipal,
           scaffoldBackgroundColor: Colors.white,
@@ -92,3 +93,26 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterSplashScreen.fadeIn(
+          backgroundColor: Colors.white,
+          onInit: () {
+            debugPrint("On Init");
+          },
+          onEnd: () {
+            debugPrint("On End");
+          },
+          childWidget: SizedBox(
+            height: 200,
+            width: 200,
+            child: Image.asset("assets/images/sections/Logo.png"),
+          ),
+          onAnimationEnd: () => debugPrint("On Fade In End"),
+          defaultNextScreen: const InitialPage()
+        );
+  }
+}
